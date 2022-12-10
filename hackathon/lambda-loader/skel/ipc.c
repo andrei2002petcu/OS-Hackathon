d3e850a9e0e0d3c08df27f0bb6d76e7581f46399
andrei2002petcu <andrei2002petcu@gmail.com>
2022-12-10 11:59:05 +0200

    Conexiune done

--- hackathon/lambda-loader/skel/ipc.c
+++ hackathon/lambda-loader/skel/ipc.c
@@ -4,22 +4,47 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
+#include <netinet/in.h>
 
 #include "ipc.h"
 
+#define PORT 5000
+struct sockaddr_in serv_addr;
+int client_fd;
 
 int create_socket()
 {
-	return -1;
+    int sock;
+
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+ 
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+	return sock;
 }
 
 int connect_socket(int fd)
 {
-	return -1;
+	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address\n");
+        return -1;
+    }
+ 
+    if ((client_fd = connect(fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+	return client_fd;
 }
 
 ssize_t send_socket(int fd, const char *buf, size_t len)
 {
+	send(fd, buf, len, 0);
+    printf("message sent\n");
+	
 	return -1;
 }
 
@@ -30,5 +55,5 @@ ssize_t recv_socket(int fd, char *buf, size_t len)
 
 void close_socket(int fd)
 {
+	close(client_fd);
 }
-
--- hackathon/lambda-loader/skel/server.c
+++ hackathon/lambda-loader/skel/server.c
@@ -6,6 +6,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <netinet/in.h>
 
 #include "ipc.h"
 #include "server.h"
@@ -13,6 +14,9 @@
 #ifndef OUTPUTFILE_TEMPLATE
 #define OUTPUTFILE_TEMPLATE "../checker/output/out-XXXXXX"
 #endif
+
+#define PORT 5000
+#define IP_ADDR "127.0.0.1"
 
 static int lib_prehooks(struct lib *lib)
 {
@@ -73,13 +77,67 @@ int main(void)
 	struct lib lib;
 
 	/* TODO - Implement server connection */
+	struct sockaddr_in addr;
+	int server_fd, val = 1;
 
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &val, sizeof(val))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(PORT);
+	addr.sin_addr.s_addr = INADDR_ANY;
+
+	if (bind(server_fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+	if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+	int addrlen = sizeof(addr), new_socket;
+	if ((new_socket = accept(server_fd, (struct sockaddr*)&addr, (socklen_t*)&addrlen)) < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+	char buffer[BUFSIZE];
 	while(1) {
 
 		/* TODO - get message from client */
+		char template[7];
+		strcpy(template, OUTPUTFILE_TEMPLATE);
+		int fd = mkstemp(template);
+		if (fd < 0) {
+			perror("file open failed");
+			exit(EXIT_FAILURE);
+		}
+
+		memset(buffer, 0, BUFSIZE);
+		read(new_socket, buffer, BUFSIZE);
+		write(fd, buffer, sizeof(buffer));
+		//printf("%s\n", buffer);
+		printf("message recieved\n");
+		close(fd);
+		break;
+		
+		// if (strcmp(buffer, "exit") == 0 || strcmp(buffer, "quit") == 0)
+		// 	break;
+		// else printf("%s\n", buffer);
+		
+
 		/* TODO - parse message with parse_command and populate lib */
 		/* TODO - handle request from client */
-		ret = lib_run(&lib);
+		//ret = lib_run(&lib);
 	}
 
 	return 0;
